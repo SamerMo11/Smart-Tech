c7f34421be3c355fe7234f5e071db95e86a64511
Ibrahemmuhhamed <hemamohame76@gmail.com>
2024-12-04 00:33:14 +0200

    Update Product

--- jsFiles/product.js
+++ jsFiles/product.js
@@ -105,6 +105,7 @@ getProduct().then((data) => {
   display();
   productPath(product.category);
   productDetails(product);
+  displayReviews(product.review);
   console.log(product);
   displayRelatedProducts(product.realtedProducts);
   displaySpecifications(product.specifications);
@@ -175,13 +176,21 @@ function productDetails(product) {
   relatedProductsSpecifations(product.combinedAttributes, product.details.id);
 }
 function displayRating(totalRating, ratingCounter, starsArr) {
-  console.log(starsArr);
-  for (let i = 0; i < totalRating; i++) {
-    starsArr[i].style.fill = "#FFAD33";
+  const parentElement = starsArr[0].parentElement.parentElement;
+  console.log(parentElement);
+  for (let i = 0; i < 5; i++) {
+    if (i < totalRating) {
+      starsArr[i].style.fill = "#FFAD33";
+    } else {
+      starsArr[i].style.fill = "#656f7e";
+    }
   }
-  document.querySelector(
-    ".rating--number"
-  ).innerHTML = `(${ratingCounter} Reviews)`;
+  if (ratingCounter) {
+    if (parentElement.querySelector(".rating--number"))
+      parentElement.querySelector(
+        ".rating--number"
+      ).innerHTML = `(${ratingCounter} Reviews)`;
+  }
 }
 function displayColors(products) {
   const colorsDiv = productSpecificationDiv("colors");
@@ -403,3 +412,99 @@ function checkIsScroll(i) {
     btn.style.display = allowScroll ? "block" : "none";
   });
 }
+function displayReviews(reviews) {
+  const reviewsDiv = document.querySelector(".reviews");
+  reviewsDiv.innerHTML = "";
+  let html = "";
+  reviews.forEach((review) => {
+    const { date, image, message, rateValue, userName } = review;
+    html += `
+     <div>
+                <img src="../products/user.png" alt="" srcset="" />
+                <div class="user--info">
+                  <p class="user--name">${userName}</p>
+                  <p class="review--text">
+                  ${message}
+                  </p>
+                </div>
+                <p class="review--date">Reviewed on ${date}</p>
+                <div class="stars">
+                  <svg
+                    width="16"
+                    height="15"
+                    viewBox="0 0 16 15"
+                    fill="#656f7e"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.673 7.17173C15.7437 6.36184 15.1709 4.65517 13.8284 4.65517H11.3992C10.7853 4.65517 10.243 4.25521 10.0617 3.66868L9.33754 1.32637C8.9309 0.0110567 7.0691 0.0110564 6.66246 1.32637L5.93832 3.66868C5.75699 4.25521 5.21469 4.65517 4.60078 4.65517H2.12961C0.791419 4.65517 0.215919 6.35274 1.27822 7.16654L3.39469 8.78792C3.85885 9.1435 4.05314 9.75008 3.88196 10.3092L3.11296 12.8207C2.71416 14.1232 4.22167 15.1704 5.30301 14.342L7.14861 12.9281C7.65097 12.5432 8.34903 12.5432 8.85139 12.9281L10.6807 14.3295C11.7636 15.159 13.2725 14.1079 12.8696 12.8046L12.09 10.2827C11.9159 9.71975 12.113 9.10809 12.5829 8.75263L14.673 7.17173Z"
+                    />
+                  </svg>
+                  <svg
+                    width="16"
+                    height="15"
+                    viewBox="0 0 16 15"
+                    fill="#656f7e"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.673 7.17173C15.7437 6.36184 15.1709 4.65517 13.8284 4.65517H11.3992C10.7853 4.65517 10.243 4.25521 10.0617 3.66868L9.33754 1.32637C8.9309 0.0110567 7.0691 0.0110564 6.66246 1.32637L5.93832 3.66868C5.75699 4.25521 5.21469 4.65517 4.60078 4.65517H2.12961C0.791419 4.65517 0.215919 6.35274 1.27822 7.16654L3.39469 8.78792C3.85885 9.1435 4.05314 9.75008 3.88196 10.3092L3.11296 12.8207C2.71416 14.1232 4.22167 15.1704 5.30301 14.342L7.14861 12.9281C7.65097 12.5432 8.34903 12.5432 8.85139 12.9281L10.6807 14.3295C11.7636 15.159 13.2725 14.1079 12.8696 12.8046L12.09 10.2827C11.9159 9.71975 12.113 9.10809 12.5829 8.75263L14.673 7.17173Z"
+                    />
+                  </svg>
+                  <svg
+                    width="16"
+                    height="15"
+                    viewBox="0 0 16 15"
+                    fill="#656f7e"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.673 7.17173C15.7437 6.36184 15.1709 4.65517 13.8284 4.65517H11.3992C10.7853 4.65517 10.243 4.25521 10.0617 3.66868L9.33754 1.32637C8.9309 0.0110567 7.0691 0.0110564 6.66246 1.32637L5.93832 3.66868C5.75699 4.25521 5.21469 4.65517 4.60078 4.65517H2.12961C0.791419 4.65517 0.215919 6.35274 1.27822 7.16654L3.39469 8.78792C3.85885 9.1435 4.05314 9.75008 3.88196 10.3092L3.11296 12.8207C2.71416 14.1232 4.22167 15.1704 5.30301 14.342L7.14861 12.9281C7.65097 12.5432 8.34903 12.5432 8.85139 12.9281L10.6807 14.3295C11.7636 15.159 13.2725 14.1079 12.8696 12.8046L12.09 10.2827C11.9159 9.71975 12.113 9.10809 12.5829 8.75263L14.673 7.17173Z"
+                    />
+                  </svg>
+                  <svg
+                    width="16"
+                    height="15"
+                    viewBox="0 0 16 15"
+                    fill="#656f7e"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.673 7.17173C15.7437 6.36184 15.1709 4.65517 13.8284 4.65517H11.3992C10.7853 4.65517 10.243 4.25521 10.0617 3.66868L9.33754 1.32637C8.9309 0.0110567 7.0691 0.0110564 6.66246 1.32637L5.93832 3.66868C5.75699 4.25521 5.21469 4.65517 4.60078 4.65517H2.12961C0.791419 4.65517 0.215919 6.35274 1.27822 7.16654L3.39469 8.78792C3.85885 9.1435 4.05314 9.75008 3.88196 10.3092L3.11296 12.8207C2.71416 14.1232 4.22167 15.1704 5.30301 14.342L7.14861 12.9281C7.65097 12.5432 8.34903 12.5432 8.85139 12.9281L10.6807 14.3295C11.7636 15.159 13.2725 14.1079 12.8696 12.8046L12.09 10.2827C11.9159 9.71975 12.113 9.10809 12.5829 8.75263L14.673 7.17173Z"
+                    />
+                  </svg>
+                  <svg
+                    width="16"
+                    height="15"
+                    viewBox="0 0 16 15"
+                    fill="#656f7e"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M14.673 7.17173C15.7437 6.36184 15.1709 4.65517 13.8284 4.65517H11.3992C10.7853 4.65517 10.243 4.25521 10.0617 3.66868L9.33754 1.32637C8.9309 0.0110567 7.0691 0.0110564 6.66246 1.32637L5.93832 3.66868C5.75699 4.25521 5.21469 4.65517 4.60078 4.65517H2.12961C0.791419 4.65517 0.215919 6.35274 1.27822 7.16654L3.39469 8.78792C3.85885 9.1435 4.05314 9.75008 3.88196 10.3092L3.11296 12.8207C2.71416 14.1232 4.22167 15.1704 5.30301 14.342L7.14861 12.9281C7.65097 12.5432 8.34903 12.5432 8.85139 12.9281L10.6807 14.3295C11.7636 15.159 13.2725 14.1079 12.8696 12.8046L12.09 10.2827C11.9159 9.71975 12.113 9.10809 12.5829 8.75263L14.673 7.17173Z"
+                    />
+                  </svg>
+                </div>
+              </div>`;
+
+    // displayRating(totalRating, ratingCounter, starsArr);
+  });
+  reviewsDiv.insertAdjacentHTML("beforeend", html);
+  const starsDivs = reviewsDiv.querySelectorAll(".stars");
+  console.log(starsDivs);
+  starsDivs.forEach((div, i) => {
+    console.log(div);
+    const stars = div.querySelectorAll("svg");
+    console.log(stars);
+    displayRating(reviews[i].rateValue, undefined, stars);
+  });
+}
+// displayRating(totalRating, ratingCounter, starsArr);
+const reviewStars = document.querySelectorAll(".user--review .stars svg");
+console.log(reviewStars);
+reviewStars.forEach((stars, i) => {
+  console.log(stars);
+  stars.addEventListener("mousemove", (e) => {
+    displayRating(i + 1, undefined, reviewStars);
+  });
+});
