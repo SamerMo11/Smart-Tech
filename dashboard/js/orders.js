b6796f91f95ae31cf42726e03b6f6b7ba01127b2
Ibrahemmuhhamed <hemamohame76@gmail.com>
2024-12-06 14:58:51 +0200

    Update Order Page

--- dashboard/js/orders.js
+++ dashboard/js/orders.js
@@ -32,8 +32,14 @@ getOrders()
     ordersDivs.forEach((orderDiv) => {
       orderDiv.addEventListener("click", (e) => {
         e.stopPropagation();
+        getOrderDetails(e.currentTarget.getAttribute("data-value")).then(
+          (data) => {
+            displayOrderDetails(data);
+            handleItemClicked(orderDiv);
+          }
+        );
 
-        handleItemClicked(orderDiv);
+        console.log(e.currentTarget.getAttribute("data-value"));
         removeActiveKababMenus();
       });
     });
@@ -44,6 +50,12 @@ getOrders()
       btn.addEventListener("click", (e) => {
         e.stopPropagation();
         console.log(btn);
+        getOrderDetails(e.currentTarget.getAttribute("data-value")).then(
+          (data) => {
+            displayOrderDetails(data);
+          }
+        );
+
         tableViewContainer.classList.add("show");
       });
     });
@@ -65,14 +77,14 @@ function displayOrders(orders) {
   for (let i = 0; i < orders.length; i++) {
     const order = orders[i];
     console.log(order);
-    html += `  <ul>
+    html += `  <ul data-value= ${order.id}>
       <li>${order.id}</li>
       <li>${order.createdDay}</li>
       <li>${order.paymentMethod}</li>
       <li class="${order.paymentStatus.toLowerCase().replace(" ", "-")}">
         <span>${order.paymentStatus}</span>
       </li>
-      <li>
+      <li class = ${order.orderStatus}>
         <span>${order.orderStatus}</span>
       </li>
       <li>$${order.total.toFixed(2)}</li>
@@ -92,7 +104,8 @@ function displayOrders(orders) {
   ordersTable.innerHTML += html;
 }
 
-const statusLis = document.querySelectorAll(".status--input li");
+const statusLis = document.querySelectorAll(".table--controllers li");
+console.log(statusLis);
 statusLis.forEach((s) =>
   s.addEventListener("click", () =>
     ordersFilter({ status: s.innerHTML }).catch((e) =>
@@ -115,4 +128,56 @@ async function ordersFilter(obj) {
   } catch (e) {
     throw e;
   }
+}
+async function getOrderDetails(orderId) {
+  try {
+    const req = await fetch("./js/details.json");
+    if (!req.ok) throw new Error("Can't Get Order Details");
+    return await req.json();
+  } catch (e) {
+    displayToast("error", e.message);
+    throw e;
+  }
+}
+function displayOrderDetails(orderDeatils) {
+  console.log(orderDeatils);
+  const updateUserInfo = () => {
+    const userName = document.querySelector(".user--overview .user--name");
+    userName.innerHTML = orderDeatils.customerName;
+    const email = document.querySelector(".user--overview .user--email");
+    email.innerHTML = orderDeatils.email;
+    const phone = document.querySelector(".user--overview .user--phone");
+    phone.innerHTML = orderDeatils.phone;
+  };
+  const updateOrderList = () => {
+    const orderListDiv = document.querySelector(".order--items .products");
+    orderListDiv.innerHTML = "";
+    const orderList = orderDeatils.orders[0]["items"];
+
+    let html = "";
+    orderList.forEach((order) => {
+      html += `<div>
+                <img src="./images/product.png" alt="Product image">
+                <p class="product--title">
+                 ${order.name} <span class="sells--value">$${order.price} <span>x${order.quantity}</span></span>
+                </p>
+              </div>`;
+    });
+    orderListDiv.insertAdjacentHTML("beforeend", html);
+  };
+  const updateOrderHead = (order) => {
+    const totalPrice = document.querySelector(
+      ".item-detail-view .price .value"
+    );
+    totalPrice.innerHTML = order.total;
+    const orderDate = document.querySelector(".order--overview .date");
+    orderDate.innerHTML = "Placed on " + order.orderDate;
+    const orderStatus = document.querySelector(
+      ".item-detail-view .order--status"
+    );
+    orderStatus.innerHTML = order.status;
+  };
+  updateUserInfo();
+  updateOrderList();
+  updateOrderHead(orderDeatils.orders[0]);
 }
